e41c322e783105dde9af9a0f391bb2b8e7389bfa
Smeaguuul <mikkelhoffmannp@gmail.com>
2024-11-01 11:38:23 +0100

    Gamestate

---
+++ Server/Gamestate.mjs
@@ -0,0 +1,88 @@
+import { chanceScore, fourOfAKindScore, fullHouseScore, largeStraightScore, onePairScore, smallStraightScore, threeOfAKindScore, twoPairScore, upperSectionScore, yatzyScore } from "./YatzyResultCalculators.mjs"
+
+
+export let gameStates = [];
+
+class gameState {
+    constructor(playerName) {
+        this.name = playerName;
+        this.turnCounter = 0;
+        this.finished = false;
+
+        //Terninger
+        this.dice = [5];
+        this.dice.forEach(index => dice[index] = new die());
+
+        //Resultlisten
+        this.results = {};
+        for (let eyes = 1; index <= 6; index++) {
+            results[eyes] = new resultLine(index + "'ere", upperSectionScore(eyes));
+        }
+        results['onePair'] = new resultLine('onePair', onePairScore(this.dice));
+        results['twoPair'] = new resultLine('twoPair', twoPairScore(this.dice));
+        results['threeOfAKinde'] = new resultLine('threeOfAKinde', threeOfAKindScore(this.dice));
+        results['fourOfAkind'] = new resultLine('fourOfAkind', fourOfAKindScore(this.dice));
+        results['fullHouseScore'] = new resultLine('fullHouseScore', fullHouseScore(this.dice));
+        results['smallStraightScore'] = new resultLine('smallStraightScore', smallStraightScore(this.dice));
+        results['largeStraightScore'] = new resultLine('largeStraightScore', largeStraightScore(this.dice));
+        results['chanceScore'] = new resultLine('chanceScore', chanceScore(this.dice));
+        results['yatzyScore'] = new resultLine('yatzyScore', yatzyScore(this.dice));
+
+        this.totalScore = (results) => {
+            let sum = 0;
+            for (const resultLine of results) {
+                sum += resultLine.score;
+            }
+            return sum;
+        }
+    }
+
+    /**
+     * 
+     * @returns up to date gameState object
+     */
+    gameState() {
+        let totalScore = 0;
+        for (const resultLine of this.results) {
+            resultLine.score = resultLine.calculater();
+            totalScore += resultLine.score;
+        }
+        this.turnCounter++;
+        if (this.turnCounter == 15) {
+            this.finished = true;
+        }
+        return { turnNr: this.turnCounter, result: { list: this.results, totalScore: totalScore }, finished: this.finished };
+    }
+
+    rollDice() {
+        for (const die of this.dice) {
+            die.roll();
+        }
+    }
+
+    diceHoldChange(index) {
+        this.dice[index].hold = !this.dice[index].hold
+    }
+}
+
+class resultLine {
+    constructor(name, calculator) {
+        this.name = name;
+        this.used = false;
+        this.score = 0;
+        this.calculater = calculator;
+    }
+}
+
+class die {
+    constructor() {
+        this.hold = false;
+        this.value = this.roll();
+    }
+
+    roll() {
+        if (die.hold != true) {
+            die.value = Math.floor(Math.random() * 6 + 1)
+        }
+    }
+}
---
+++ Server/YatzyResultCalculators.mjs
@@ -0,0 +1,108 @@
+export function upperSectionScore(eyes) {
+    return () => {
+        let counter = 0
+        for (const die of diceArray) {
+            if (die.value == eyes) {
+                counter++
+            }
+        }
+        return counter * eyes
+    }
+}
+
+
+function findMathingEyes(diceArray, excludedPairSum, matchAmount) {
+    let sumPair = 0
+
+    let rolls = [0, 0, 0, 0, 0, 0]
+    for (let die of diceArray) {
+        rolls[die.value - 1] += 1
+    }
+
+    for (let index = 0; index < rolls.length; index++) {
+        if (rolls[index] >= matchAmount && (index + 1) * matchAmount != excludedPairSum) {
+            sumPair = matchAmount * (index + 1)
+        }
+    }
+    return sumPair
+}
+
+
+export function onePairScore(dice) {
+    return () => findMathingEyes(dice, 0, 2)
+}
+
+export function twoPairScore(dice) {
+    return () => {
+        let firstPair = findMathingEyes(dice, 0, 2)
+        let secondPair = findMathingEyes(dice, firstPair, 2)
+        let twoPairScore = 0
+        if (firstPair != 0 && secondPair != 0) {
+            twoPairScore = firstPair + secondPair
+        }
+        return twoPairScore
+    }
+}
+
+export function threeOfAKindScore(dice) {
+    return () => findMathingEyes(dice, 0, 3)
+}
+
+export function fourOfAKindScore() {
+    return () => findMathingEyes(dice, 0, 4)
+}
+
+
+export function smallStraightScore(dice) {
+    return () => checkInARow(dice, 4) ? 15 : 0
+}
+
+export function checkInARow(diceArray, inARow) {
+    let straightPossible = false
+    let sortedDice = Array.from(new Set(diceArray)).sort((a, b) => a - b)
+    let consecutives = 1;
+    for (let index = 1; index < sortedDice.length; index++) {
+        if (sortedDice[index] - sortedDice[index - 1] === 1) {
+            consecutives++
+            if (consecutives == inARow) {
+                straightPossible = true;
+            }
+        }
+        else {
+            consecutives = 1;
+        }
+    }
+    return straightPossible
+}
+
+export function largeStraightScore() {
+    return () => checkInARow(dice, 5) ? 20 : 0
+}
+
+
+export function fullHouseScore(dice) {
+    return () => {
+        let threeOfAkind = findMathingEyes(dice, 0, 3);
+        let excludeSum = (threeOfAkind * 2) / 3;
+        let twoOfAKind = findMathingEyes(dice, excludeSum, 2);
+        let totalSum = 0;
+        if (threeOfAkind != 0 && twoOfAKind != 0) {
+            totalSum = threeOfAkind + twoOfAKind;
+        }
+        return totalSum;
+    }
+}
+
+export function chanceScore(diceArray) {
+    return () => {
+        let chance = 0;
+        for (const die of diceArray) {
+            chance += die.value
+        }
+        return chance;
+    }
+}
+
+export function yatzyScore(dice) {
+    return () => findMathingEyes(dice, 0, 5) > 0 ? 50 : 0
+}
